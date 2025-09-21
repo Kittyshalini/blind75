a4e7f5acc9e3ded99c17bc95fe3a02a2f2db370a
Kittyshalini <83652778+Kittyshalini@users.noreply.github.com>
2025-09-21 23:27:03 +0530

    Completed TwoSum

---
+++ Arrays/1twoSum/twoSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+        vector<int> v;
+        for(int i=0;i<nums.size();i++){
+            auto it = m.find(target-nums[i]);
+            if(it != m.end()){
+                v.push_back(it->second);
+                v.push_back(i);
+            }
+            else{
+                m.insert({nums[i],i});
+            }
+        }
+        return(v);
+    }
+};
